56b9e4178d94f2ce6c0f3bcb54df916d6e2fd29e
Ruby Chen <rc071404@bu.edu>
2025-03-29 14:21:12 -0400

    added some tests for ring_model.py for entering the ring

--- HW/HW2 Codebase/boxing/tests/test_ring_model.py
+++ HW/HW2 Codebase/boxing/tests/test_ring_model.py
@@ -18,6 +18,7 @@ def ring_model():
     """Provides a new instance of RingModel."""
     return RingModel()
 
+"""Fixtures that provide a sample boxer"""
 @pytest.fixture
 def sample_boxer_1():
     return Boxer(id=1, name="Ali", weight=150, height=70, reach=72.5, age=30)
@@ -27,8 +28,29 @@ def sample_boxer_2():
     return Boxer(id=2, name="Tyson", weight=160, height=72, reach=73.0, age=32)
 
 
-#testing enter_ring constructor 
-def test_enter_ring_adds_boxer(ring_model, sample_boxer_1):
+
+# ------------------
+# TESTS FOR ENTERING THE RING 
+# ------------------
+
+def test_enter_ring_adds_first_boxer(ring_model, sample_boxer_1):
     ring_model.enter_ring(sample_boxer_1)
     assert len(ring_model.ring) == 1
     assert ring_model.ring[0].name == "Ali"
+
+def test_enter_ring_adds_second_boxer(ring_model, sample_boxer_2):
+    ring_model.enter_ring(sample_boxer_1)
+    ring_model.enter_ring(sample_boxer_2)
+    assert len(ring_model.ring) == 2
+    assert ring_model.ring[1].name == "Tyson"
+
+def test_enter_ring_not_boxer(ring_model):
+    with pytest.raises(TypeError, match="Invalid input, Expected 'Boxer'"):
+        ring_model.enter_ring("not_a_boxer")
+
+def test_enter_ring_raises_error_when_full(ring_model, sample_boxer_1, sample_boxer_2):
+    """Test that a ValueError is raised when trying to add more than 2 boxers."""
+    ring_model.enter_ring(sample_boxer_1)
+    ring_model.enter_ring(sample_boxer_2)
+    with pytest.raises(ValueError, match="The ring is at max capacity."):
+        ring_model.enter_ring(sample_boxer_1)
